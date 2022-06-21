87fe7491608e15e4a6e2bcd83d6e96ea016b3c65
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-06-21 18:50:10 +0100

    Finished MVP CRUD for portolios

--- models/portfolios.js
+++ models/portfolios.js
@@ -24,10 +24,19 @@ export async function getPortfoliosByKeyword(keyword) {
   return res.rows;
 }
 
+//return portfolio by id
+export async function getPortfolioById(id) {
+  const res = await query(
+    "SELECT * FROM portfolios WHERE portfolios.portfolio_id = $1;",
+    [id]
+  );
+  return res.rows;
+}
+
 //add new portfolio
 export async function addNewPortfolio(portfolio) {
   const res = await query(
-    "INSERT INTO portfolios (site_url, site_image, description, experience_level, keyword_search, voting_score) VALUES ($1, $2, $3, $4, $5, $6);",
+    "INSERT INTO portfolios (site_url, site_image, description, experience_level, keyword_search, voting_score) VALUES ($1, $2, $3, $4, $5, $6) RETURNING*;",
     [
       portfolio.url,
       portfolio.image,
@@ -45,5 +54,63 @@ export async function deletePortfolio(reference) {
   const res = await query(
     "DELETE FROM portfolios WHERE portfolio_id = $1 RETURNING*;",
     [reference]
+  );
+}
+
+//fully update a portfolio
+export async function fullUpdateOfPortfolio(id, portfolio) {
+  const res = await query(
+    "UPDATE portfolios SET site_url = $1, site_image = $2, description = $3, experience_level = $4, keyword_search = $5, voting_score = $6 WHERE portfolio_id = $7 RETURNING*;",
+    [
+      portfolio.url,
+      portfolio.image,
+      portfolio.description,
+      portfolio.experience,
+      portfolio.keyword,
+      portfolio.voting,
+      id,
+    ]
+  );
+  return res.rows;
+}
+
+//following functions are for specific updates or an entry as per name
+//update URL of portfolio
+export async function UpdateURLPortfolio(id, value) {
+  const res = await query(
+    "UPDATE portfolios SET site_url = $1 WHERE portfolio_id = $2 RETURNING*;",
+    [value, id]
+  );
+}
+
+//update image of portfolio
+export async function UpdateImagePortfolio(id, value) {
+  const res = await query(
+    "UPDATE portfolios SET site_image = $1 WHERE portfolio_id = $2 RETURNING*;",
+    [value, id]
+  );
+}
+
+//update description of portfolio
+export async function UpdateDescriptionPortfolio(id, value) {
+  const res = await query(
+    "UPDATE portfolios SET description = $1 WHERE portfolio_id = $2 RETURNING*;",
+    [value, id]
+  );
+}
+
+//update experience_level of portfolio
+export async function UpdateExpPortfolio(id, value) {
+  const res = await query(
+    "UPDATE portfolios SET experience_level = $1 WHERE portfolio_id = $2 RETURNING*;",
+    [value, id]
+  );
+}
+
+//update keyword search of portfolio
+export async function UpdateKeywordPortfolio(id, value) {
+  const res = await query(
+    "UPDATE portfolios SET keyword_search = $1 WHERE portfolio_id = $2 RETURNING*;",
+    [value, id]
   );
 }
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -6,35 +6,77 @@ import {
   getPortfoliosByKeyword,
   addNewPortfolio,
   deletePortfolio,
+  fullUpdateOfPortfolio,
+  getPortfolioById,
+  UpdateImagePortfolio,
+  UpdateURLPortfolio,
+  UpdateKeywordPortfolio,
+  UpdateExpPortfolio,
+  UpdateDescriptionPortfolio,
 } from "../models/portfolios.js";
 
 router.get("/", async function (req, res) {
-  //search for keyword
+  //search for keyword and return all portfolios
   if (req.query.keyword !== undefined) {
     const result = await getPortfoliosByKeyword(req.query.keyword);
     return res.json({ success: true, payload: result });
   }
-  //search for exp level
+  //search for exp level and return all portfolios
   if (req.query.experience !== undefined) {
     const result = await getPortfoliosByExLevel(req.query.experience);
     return res.json({ success: true, payload: result });
   }
-  //returns all if no specific search done
+  //returns all portfolios if no specific search done
   const result = await getPortfolios();
   res.json({ success: true, payload: result });
 });
 
 //add portfolio
 router.post("/", async function (req, res) {
-  console.log(req.body);
   const result = await addNewPortfolio(req.body);
-  console.log('result ' + result)
+  return res.json({ success: true, payload: result });
+});
+
+//search for specific portfolio
+router.get("/:id", async function (req, res) {
+  const result = await getPortfolioById(Number(req.params.id));
   return res.json({ success: true, payload: result });
 });
 
 //delete portfolio
 router.delete("/:id", async function (req, res) {
   const result = await deletePortfolio(Number(req.params.id));
+  return res.json({ success: true, payload: result });
+});
+
+//full update of specific portfolio
+router.put("/:id", async function (req, res) {
+  const result = await fullUpdateOfPortfolio(Number(req.params.id), req.body);
+  return res.json({ success: true, payload: result });
+});
+
+//partial update of specific portfolio
+router.patch("/:id", async function (req, res) {
+  const keys = Object.keys(req.body);
+  const value = Object.values(req.body);
+  let result = "";
+
+  //checks what has been requested to be updated and calls appropriate function
+  if (keys[0] === "image") {
+    result = await UpdateImagePortfolio(Number(req.params.id), value[0]);
+  }
+  if (keys[0] === "url") {
+    result = await UpdateURLPortfolio(Number(req.params.id), value[0]);
+  }
+  if (keys[0] === "description") {
+    result = await UpdateDescriptionPortfolio(Number(req.params.id), value[0]);
+  }
+  if (keys[0] === "experience") {
+    result = await UpdateExpPortfolio(Number(req.params.id), value[0]);
+  }
+  if (keys[0] === "keyword") {
+    result = await UpdateKeywordPortfolio(Number(req.params.id), value[0]);
+  }
   return res.json({ success: true, payload: result });
 });
 
