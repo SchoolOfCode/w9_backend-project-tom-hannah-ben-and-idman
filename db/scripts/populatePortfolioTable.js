6815ea05a1d4f9830556e848ed85028aa8d20cdc
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-06-22 14:15:50 +0100

    Added holding portfolios for db

--- db/scripts/populatePortfolioTable.js
+++ db/scripts/populatePortfolioTable.js
@@ -1,13 +1,22 @@
 import { query } from "../index.js";
 import "dotenv/config";
 //import portfolio data
+import portfolios from "./portfolios.js";
 
 //finish when we have data file done
 async function populatePortfolioTable() {
-  for (let i = 0; i < Portfolio.length; i++) {
+  for (let i = 0; i < portfolios.length; i++) {
     let res = await query(
       `INSERT INTO portfolios (site_url, site_image, description, experience_level, keywords, voting_score, designers_name) VALUES ($1, $2, $3, $4, $5, $6, $7);`,
-      []
+      [
+        portfolios[i].site_url,
+        portfolios[i].site_image,
+        portfolios[i].description,
+        portfolios[i].experience_level,
+        portfolios[i].keywords,
+        portfolios[i].voting_score,
+        portfolios[i].designers_name,
+      ]
     );
   }
 }
---
+++ db/scripts/portfolios.js
@@ -0,0 +1,36 @@
+let portfolios = [
+  {
+    site_url: "https://www.eunjeongyoo.com/",
+    site_image:
+      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
+    description: "graphic designer",
+    experience_level: "high",
+    keywords: "stylish art",
+  },
+  {
+    site_url: "https://www.eunjeongyoo.com/",
+    site_image:
+      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
+    description: "graphic designer",
+    experience_level: "high",
+    keywords: "stylish art",
+  },
+  {
+    site_url: "https://www.eunjeongyoo.com/",
+    site_image:
+      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
+    description: "graphic designer",
+    experience_level: "high",
+    keywords: "stylish art",
+  },
+  {
+    site_url: "https://www.eunjeongyoo.com/",
+    site_image:
+      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
+    description: "graphic designer",
+    experience_level: "high",
+    keywords: "stylish art",
+  },
+];
+
+export default portfolios;
