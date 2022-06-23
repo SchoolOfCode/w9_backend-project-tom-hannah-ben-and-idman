7763f1f4983a4d389b06b6fb54a2f639a9ae9805
TomBirbeck <tombirbeck47@gmail.com>
2022-06-23 13:28:47 +0100

    remove profiles that didn't work

--- db/scripts/portfolios.js
+++ db/scripts/portfolios.js
@@ -9,32 +9,69 @@ let portfolios = [
     designers_name: "eunjeongyoo",
   },
   {
-    site_url: "https://www.eunjeongyoo.com/",
-    site_image:
-      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
-    description: "graphic designer",
-    experience_level: "high",
-    keywords: "stylish art",
-    designers_name: "eunjeongyoo",
+    site_url: "https://dribbble.com/lapatun_dolka",
+    site_image: "https://cdn.dribbble.com/users/2422127/screenshots/14523783/media/9d8061753c098e73cef420894b58a46f.gif",
+    designers_name: "Alexander Dolnikov",
+    keywords: "animation",
+    description: "2D animation",
+    experience_level: "Intermediate"
+      },
+  {
+site_url: "http://www.hm-creative.com/",
+site_image: "https://cdn.dribbble.com/userupload/2950300/file/original-a77d596a12e645083835f4581110909e.png?resize=1612x1210",
+designers_name: "Helena Olson",
+keywords: "Typography",
+description: "Free lance graphic designer",
+experience_level: "Pro"
   },
   {
-    site_url: "https://www.eunjeongyoo.com/",
-    site_image:
-      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
-    description: "graphic designer",
-    experience_level: "high",
-    keywords: "stylish art",
-    designers_name: "eunjeongyoo",
+site_url: "https://jordanhughes.co/",
+site_image: "https://d33wubrfki0l68.cloudfront.net/0eb1d64625bc1f82104d6407acea2780b503e499/2c1e8/images/upguard-feature_1.jpg",
+designers_name: "Jordan Hughes",
+keywords: "UX/UI",
+description: "UX/UI design",
+experience_level: "Pro"
   },
   {
-    site_url: "https://www.eunjeongyoo.com/",
-    site_image:
-      "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
-    description: "graphic designer",
-    experience_level: "high",
-    keywords: "stylish art",
-    designers_name: "eunjeongyoo",
+site_url: "https://dribbble.com/germanirina",
+site_image: "https://cdn.dribbble.com/users/2034980/screenshots/18222773/media/d83a47c5d6f6328f0c82123e1c727a05.png?compress=1&resize=450x338&vertical=top",
+designers_name: "Irina German",
+keywords: "Web design",
+description: "Graphic designer",
+experience_level: "Pro"
   },
+  {
+site_url: "https://www.joebentaylor.co.uk/",
+site_image: "https://cdn.dribbble.com/users/432077/screenshots/18060224/media/e22dabea4bb4a47f959b8f89ef940ae9.jpg?compress=1&resize=450x338&vertical=top",
+designers_name: "Joe Taylor",
+keywords: "Branding",
+description: "Logo Specialist, Web Designer & Developer",
+experience_level: "Intermediate"
+  },
+  {
+site_url: "https://www.behance.net/524cb354",
+site_image: "https://mir-s3-cdn-cf.behance.net/projects/max_808/217edb145688147.Y3JvcCwxMDYwLDgyOSwxNjUsNzM.jpg",
+designers_name: "Nora Tsiteladze",
+keywords: "UX/UI",
+description: "UX/UI designer",
+experience_level: "Intermediate"
+  },
+  {
+site_url: "https://evabochem-shur.com/",
+site_image: "https://freight.cargo.site/w/1500/q/75/i/12abf1a54023b60078816f45487cc7e0383ac111f59e7960d4dac01958b7122d/EvaBochem-Shur_CapeCodthumb-05.jpg",
+designers_name: "Eva Bochem-Shur",
+keywords: "Branding",
+description: "Independent art director and graphic designer",
+experience_level: "Intermediate"
+  },
+  {
+site_url: "https://dribbble.com/mooqlla",
+site_image: "https://cdn.dribbble.com/users/5124613/screenshots/18516258/media/5e1c6450e295744a17ce76c8f8cfe0c2.gif",
+designers_name: "Anastasija",
+keywords: "animation",
+description: "Digital Illustrator",
+experience_level: "Intermediate"
+  }
 ];
 
 export default portfolios;
