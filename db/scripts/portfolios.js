107510abba69e8380ba9ad788c910b56967d8f43
TomBirbeck <tombirbeck47@gmail.com>
2022-06-24 10:40:10 +0100

    removed bad portfolio

--- db/scripts/portfolios.js
+++ db/scripts/portfolios.js
@@ -4,7 +4,7 @@ let portfolios = [
     site_image:
       "https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/60edb894d267c8880e267733_1.jpg",
     description: "graphic designer",
-    experience_level: "high",
+    experience_level: "Experienced",
     keywords: "stylish art",
     designers_name: "eunjeongyoo",
   },
@@ -14,7 +14,7 @@ let portfolios = [
     designers_name: "Alexander Dolnikov",
     keywords: "animation",
     description: "2D animation",
-    experience_level: "Intermediate"
+    experience_level: "Experienced"
       },
   {
 site_url: "http://www.hm-creative.com/",
@@ -22,7 +22,7 @@ site_image: "https://cdn.dribbble.com/userupload/2950300/file/original-a77d596a1
 designers_name: "Helena Olson",
 keywords: "Typography",
 description: "Free lance graphic designer",
-experience_level: "Pro"
+experience_level: "Experienced"
   },
   {
 site_url: "https://jordanhughes.co/",
@@ -30,7 +30,7 @@ site_image: "https://d33wubrfki0l68.cloudfront.net/0eb1d64625bc1f82104d6407acea2
 designers_name: "Jordan Hughes",
 keywords: "UX/UI",
 description: "UX/UI design",
-experience_level: "Pro"
+experience_level: "Experienced"
   },
   {
 site_url: "https://dribbble.com/germanirina",
@@ -38,7 +38,7 @@ site_image: "https://cdn.dribbble.com/users/2034980/screenshots/18222773/media/d
 designers_name: "Irina German",
 keywords: "Web design",
 description: "Graphic designer",
-experience_level: "Pro"
+experience_level: "Experienced"
   },
   {
 site_url: "https://www.joebentaylor.co.uk/",
@@ -46,7 +46,7 @@ site_image: "https://cdn.dribbble.com/users/432077/screenshots/18060224/media/e2
 designers_name: "Joe Taylor",
 keywords: "Branding",
 description: "Logo Specialist, Web Designer & Developer",
-experience_level: "Intermediate"
+experience_level: "Experienced"
   },
   {
 site_url: "https://www.behance.net/524cb354",
@@ -54,7 +54,7 @@ site_image: "https://mir-s3-cdn-cf.behance.net/projects/max_808/217edb145688147.
 designers_name: "Nora Tsiteladze",
 keywords: "UX/UI",
 description: "UX/UI designer",
-experience_level: "Intermediate"
+experience_level: "Experienced"
   },
   {
 site_url: "https://evabochem-shur.com/",
@@ -62,7 +62,7 @@ site_image: "https://freight.cargo.site/w/1500/q/75/i/12abf1a54023b60078816f4548
 designers_name: "Eva Bochem-Shur",
 keywords: "Branding",
 description: "Independent art director and graphic designer",
-experience_level: "Intermediate"
+experience_level: "Experienced"
   },
   {
 site_url: "https://dribbble.com/mooqlla",
@@ -70,8 +70,75 @@ site_image: "https://cdn.dribbble.com/users/5124613/screenshots/18516258/media/5
 designers_name: "Anastasija",
 keywords: "animation",
 description: "Digital Illustrator",
-experience_level: "Intermediate"
-  }
+experience_level: "Experienced"
+  },
+  {
+site_url: "https://www.upgrad.com/blog/react-project-ideas-topics-beginners/",
+site_image: "https://www.upgrad.com/blog/wp-content/uploads/2020/05/1__kpXka6VYTtrGqxE-q0Ozg.png",
+designers_name: "Rohan Vats",
+keywords: "chart",
+description: "Beginner friendly projects",
+experience_level: "Beginner"
+  },
+  {
+site_url: "https://flatlogic.com/blog/6-stunning-react-js-project-ideas/",
+site_image: "https://833250.smushcdn.com/1694534/wp-content/uploads/2020/06/habitica-min-1024x555.png?lossy=1&strip=1&webp=1",
+designers_name: "Nastassia Ovchinnikova",
+keywords: "Productivity",
+description: "Productivity chart idea",
+experience_level: "Mid-level"
+  },
+  {
+site_url: "https://zofiakorcz.pl/mastermind-react-es6-webpack",
+site_image: "https://react.rocks/images/converted/mastermind_tight.jpg",
+designers_name: "Zosia Korcz",
+keywords: "Games",
+description: "Small Mastermind game",
+experience_level: "Beginner"
+  },
+  {
+site_url: "https://react.rocks/example/react-thermometer",
+site_image: "https://react.rocks/images/converted/react-thermometer.jpg",
+designers_name: "Conor Hastings",
+keywords: "Chart",
+description: "",
+experience_level: "Beginner"
+  },
+  {
+site_url: "https://react.rocks/example/shirt-sizes",
+site_image: "https://react.rocks/images/converted/shirt-sizes.jpg",
+designers_name: "Quang V",
+keywords: "async",
+description: "",
+experience_level: "Beginner"
+  },
+  {
+site_url: "https://surajondev.com/2021/04/27/5-react-based-projects-every-beginner-must-try/",
+site_image: "https://res.cloudinary.com/practicaldev/image/fetch/s--J4f1ublm--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/i/zdula4zwasjf68dlnuw4.PNG",
+designers_name: "Suraj Vishwakarma",
+keywords: "Recipes",
+description: "",
+experience_level: "Beginner"
+  },
+  {
+site_url: "https://dev.to/javinpaul/8-projects-beginners-can-build-to-learn-reactjs-in-2022-1e3i",
+site_image: "https://res.cloudinary.com/practicaldev/image/fetch/s--INXm52ys--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://cdn-media-1.freecodecamp.org/images/kbYsxsxb2D7mBhdlEmUrpMhRmOcQoR79vtT1",
+designers_name: "Javin Paul",
+keywords: "Movies",
+description: "",
+experience_level: "Beginner"
+  },
+  {
+site_url: "https://devchallenges.io/challenges/TtUjDt19eIHxNQ4n5jps",
+site_image: "https://res.cloudinary.com/practicaldev/image/fetch/s--AGPyAcsk--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://firebasestorage.googleapis.com/v0/b/devchallenges-1234.appspot.com/o/challengesDesigns%252FJobSearchThumbnail.png%3Falt%3Dmedia%26token%3D59d40095-f280-478f-a8c9-dc9d49f14471",
+designers_name: "Thu Nghiem",
+keywords: "Photos",
+description: "",
+experience_level: "Mid-level"
+  },
 ];
 
 export default portfolios;
+
+//Beginner, Mid-level, Experienced
+
