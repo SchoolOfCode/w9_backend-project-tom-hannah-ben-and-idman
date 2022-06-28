83dd1f9769d6935e6dec3efbf3c408d1c359e1e3
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-06-28 12:17:08 +0100

    removed comments, added comment to code not in use, prettified formatting

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,103 +1,93 @@
 import express from "express";
 const router = express.Router();
 import {
-  getPortfolios,
-  getPortfoliosByExLevel,
-  getPortfoliosByKeyword,
-  addNewPortfolio,
-  deletePortfolio,
-  fullUpdateOfPortfolio,
-  getPortfolioById,
-  UpdateImagePortfolio,
-  UpdateURLPortfolio,
-  UpdateKeywordPortfolio,
-  UpdateExpPortfolio,
-  UpdateDescriptionPortfolio,
-  getPortfoliosByDesigner,
+	getPortfolios,
+	getPortfoliosByExLevel,
+	getPortfoliosByKeyword,
+	addNewPortfolio,
+	deletePortfolio,
+	fullUpdateOfPortfolio,
+	getPortfolioById,
+	UpdateImagePortfolio,
+	UpdateURLPortfolio,
+	UpdateKeywordPortfolio,
+	UpdateExpPortfolio,
+	UpdateDescriptionPortfolio,
+	getPortfoliosByDesigner,
 } from "../models/portfolios.js";
 
 router.get("/", async function (req, res) {
-  //search for keyword and return all portfolios
-  if (req.query.keyword !== undefined) {
-    const result = await getPortfoliosByKeyword(req.query.keyword);
-    return res.json({ success: true, payload: result });
-  }
-  //search for exp level and return all portfolios
-  if (req.query.experience !== undefined) {
-    const result = await getPortfoliosByExLevel(req.query.experience);
-    return res.json({ success: true, payload: result });
-  }
-  //search for designer name and return all portfolios
-  if (req.query.designer !== undefined) {
-    const result = await getPortfoliosByDesigner(req.query.designer);
-    return res.json({ success: true, payload: result });
-  }
-  //returns all portfolios if no specific search done
-  const result = await getPortfolios();
-  res.json({ success: true, payload: result });
+	//search for keyword and return all portfolios
+	if (req.query.keyword !== undefined) {
+		const result = await getPortfoliosByKeyword(req.query.keyword);
+		return res.json({ success: true, payload: result });
+	}
+	//search for exp level and return all portfolios
+	if (req.query.experience !== undefined) {
+		const result = await getPortfoliosByExLevel(req.query.experience);
+		return res.json({ success: true, payload: result });
+	}
+	//search for designer name and return all portfolios
+	if (req.query.designer !== undefined) {
+		const result = await getPortfoliosByDesigner(req.query.designer);
+		return res.json({ success: true, payload: result });
+	}
+	//returns all portfolios if no specific search done
+	const result = await getPortfolios();
+	res.json({ success: true, payload: result });
 });
 
 //add portfolio
 router.post("/", async function (req, res) {
-  const result = await addNewPortfolio(req.body);
-  return res.status(201).json({ success: true, payload: result });
+	const result = await addNewPortfolio(req.body);
+	return res.status(201).json({ success: true, payload: result });
 });
 
 //search for specific portfolio
 router.get("/:id", async function (req, res) {
-  const result = await getPortfolioById(Number(req.params.id));
-  return res.json({ success: true, payload: result });
+	const result = await getPortfolioById(Number(req.params.id));
+	return res.json({ success: true, payload: result });
 });
-
-//search for specific portfolio
-// router.get("/:id", async function (req, res) {
-//   const result = Number(req.params.id)
-//   const portfolio = await getPortfolioById(result);
-//   console.log(portfolio)
-//   if (portfolio.payload) {
-//     return res.status(404).json({
-//       success: false,
-//       reason: `No portfolio with the ID ${result} was found!`,
-//     });
-//   }
-//   return res.json({ success: true, payload: portfolio });
-// });
 
 //delete portfolio
 router.delete("/:id", async function (req, res) {
-  const result = await deletePortfolio(Number(req.params.id));
-  return res.json({ success: true, message: "deleted portfolio"});
+	const result = await deletePortfolio(Number(req.params.id));
+	return res.json({ success: true, message: "deleted portfolio" });
 });
 
 // //full update of specific portfolio
 router.put("/:id", async function (req, res) {
-  const result = await fullUpdateOfPortfolio(Number(req.params.id), req.body);
-  return res.json({ success: true, payload: result });
+	const result = await fullUpdateOfPortfolio(Number(req.params.id), req.body);
+	return res.json({ success: true, payload: result });
 });
 
+// Below code (router.patch) is not used in current build (2022.06.28), kept for possible future feature updates
 //partial update of specific portfolio
 router.patch("/:id", async function (req, res) {
-  const keys = Object.keys(req.body);
-  const value = Object.values(req.body);
-  let result = "";
+	const keys = Object.keys(req.body);
+	const value = Object.values(req.body);
+	let result = "";
 
-  //checks what has been requested to be updated and calls appropriate function
-  if (keys[0] === "site_image") {
-    result = await UpdateImagePortfolio(Number(req.params.id), value[0]);
-  }
-  if (keys[0] === "site_url") {
-    result = await UpdateURLPortfolio(Number(req.params.id), value[0]);
-  }
-  if (keys[0] === "description") {
-    result = await UpdateDescriptionPortfolio(Number(req.params.id), value[0]);
-  }
-  if (keys[0] === "experience_level") {
-    result = await UpdateExpPortfolio(Number(req.params.id), value[0]);
-  }
-  if (keys[0] === "keywords") {
-    result = await UpdateKeywordPortfolio(Number(req.params.id), value[0]);
-  }
-  return res.json({ success: true, payload: result });
+	//checks what has been requested to be updated and calls appropriate function
+	if (keys[0] === "site_image") {
+		result = await UpdateImagePortfolio(Number(req.params.id), value[0]);
+	}
+	if (keys[0] === "site_url") {
+		result = await UpdateURLPortfolio(Number(req.params.id), value[0]);
+	}
+	if (keys[0] === "description") {
+		result = await UpdateDescriptionPortfolio(
+			Number(req.params.id),
+			value[0]
+		);
+	}
+	if (keys[0] === "experience_level") {
+		result = await UpdateExpPortfolio(Number(req.params.id), value[0]);
+	}
+	if (keys[0] === "keywords") {
+		result = await UpdateKeywordPortfolio(Number(req.params.id), value[0]);
+	}
+	return res.json({ success: true, payload: result });
 });
 
 export default router;
